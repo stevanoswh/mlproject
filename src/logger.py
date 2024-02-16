cca619f5f6de10d03b4a41a8f72e4cce3cf49952
unknown <stevanoswh@gmail.com>
2024-02-17 06:03:46 +0700

    add data ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path =  os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path =  os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path, exist_ok=True)
 
 
@@ -16,5 +16,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     logging.info("Logging has started")
